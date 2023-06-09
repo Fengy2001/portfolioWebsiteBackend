56de68a11be015cfe4a7af6c8d5f9ed234fe75f4
Derpydinosuar <99842837+Fengy2001@users.noreply.github.com>
2023-06-09 15:31:12 -0600

    finish refactoring

--- portfolioWebsiteBackend/urls.py
+++ portfolioWebsiteBackend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for portfolioWebsite project.
+URL configuration for portfolioWebsiteBackend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('numericalAnalysis/', include("numericalAnalysis.urls")),
+    path('', include("numericalAnalysis.urls")),
 ]
